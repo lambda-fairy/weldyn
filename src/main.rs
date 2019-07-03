f22ec02c58d0a23c2fa0a0063714aeb1b6a79518
Chris Wong <lambda.fairy@gmail.com>
2019-07-03 22:07:05 +1200

    Handshake!

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,41 @@
 #![feature(async_await)]
 
+use byteorder::{ByteOrder, NetworkEndian};
 use futures::prelude::*;
-use runtime::net::TcpListener;
+use runtime::net::{TcpListener, TcpStream};
+use std::io;
+use std::net::Shutdown;
 
 #[runtime::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
     let mut listener = TcpListener::bind("127.0.0.1:15000")?;
     println!("listening on {}", listener.local_addr()?);
 
     listener
         .incoming()
-        .try_for_each_concurrent(None, async move |stream| {
+        .try_for_each_concurrent(None, async move |mut stream| {
             runtime::spawn(async move {
                 println!("accepting from: {}", stream.peer_addr()?);
-
-                let (reader, writer) = &mut stream.split();
-                reader.copy_into(writer).await?;
-
-                Ok::<(), std::io::Error>(())
+                handshake(&mut stream).await?;
+                stream.shutdown(Shutdown::Both)?;
+                Ok::<(), io::Error>(())
             })
             .await
         })
         .await
+}
+
+async fn handshake(stream: &mut TcpStream) -> io::Result<()> {
+    let mut client_handshake = [0u8; 4];
+    stream.read_exact(&mut client_handshake).await?;
+    if &client_handshake != b"\0\0\0\0" {
+        println!("incorrect handshake");
+        return Ok(());
+    }
+
+    let mut server_handshake = [0u8; 4];
+    NetworkEndian::write_u32(&mut server_handshake, 42);
+    stream.write_all(&server_handshake).await?;
+
+    Ok(())
 }
